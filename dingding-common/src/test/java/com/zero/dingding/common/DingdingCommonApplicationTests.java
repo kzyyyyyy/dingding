c9b2cd116474a50f63ad278b4cd6999cbe2af273
kzyyyyy <a87977195@qq.com>
2024-01-08 23:31:48 +0800

    添加pom

---
+++ dingding-common/src/test/java/com/zero/dingding/common/DingdingCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.dingding.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingdingCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dingding-security/src/test/java/com/zero/dingdingsecurity/DingdingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.dingdingsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingdingSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
